064aa3df5feb0271981699db8ee17cd9dd8213a4
charles-augustin <charlesjebalitherson@gmail.com>
2020-01-28 11:48:33 -0500

    Modified Status API - Reserve router

--- routes/reserveRouter.js
+++ routes/reserveRouter.js
@@ -9,20 +9,40 @@ reserveRouter.use(bodyParser.json());
 reserveRouter.route('/findReservationByVehicle/:vehicleID')
     .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200 })
     .get(cors.cors, (req, res, next) => {
-        Reserve.findOne({ vehicle: req.params.vehicleID })
+        Reserve.find({ vehicle: req.params.vehicleID })
             .populate('vehicle')
+            .populate('client')
             .then((reserve) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
                 res.json(reserve);
             }, err => next(err))
             .catch(err => next(err));
+    });
+
+reserveRouter.route('/updateStatus/:reserveID')
+    .options(cors.corsWithOptions, (req, res) => {res.sendStatus = 200 })
+    .put(cors.corsWithOptions, (req, res, next) => {
+        Reserve.findByIdAndUpdate(req.params.reserveID, {
+            $set: req.body
+        }, {
+            new: true
+        })
+            .then((result) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(result);
+            }, err => next(err))
+            .catch(err => next(err))
     })
 
 reserveRouter.route('/')
     .options(cors.corsWithOptions, (req, res) => { res.sendStatus = 200; })
     .get(cors.cors, (req, res, next) => {
         Reserve.find(req.query)
+            .populate('vehicle')
+            .populate('client')
+            .in('status', 'reserved')
             .then((reserve) => {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'application/json');
